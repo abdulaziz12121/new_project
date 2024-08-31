457ee124cb2c591896dc7669062941a91203980b
abdulaziz <abdulaziz125000f@gmail.com>
2024-08-31 15:28:02 +0300

    Abdulaziz

--- main.py
+++ main.py
@@ -47,18 +47,14 @@
 #     data = preprocessing(input_features)
 #     y_pred = model.predict(data)
 #     return {"pred": y_pred.tolist()[0]}
-
-from fastapi import FastAPI, HTTPException
-import pickle
-from pydantic import BaseModel
 from fastapi import FastAPI
-import uvicorn
+from pydantic import BaseModel
 import joblib
 import numpy as np
 import streamlit as st
+
 # Load the trained model
-# Load the trained model
-model = st.joblib.load("DBSCAN_model.joblib")
+model = joblib.load("DBSCAN_model.joblib")
 
 # Streamlit app code
 st.write("Hello")
