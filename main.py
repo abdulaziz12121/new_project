35cc5a73de2b3da3bc4223d0b5bbed66e9178598
abdulaziz <abdulaziz125000f@gmail.com>
2024-08-31 15:48:13 +0300

    Abdulaziz

--- main.py
+++ main.py
@@ -59,24 +59,24 @@ model = joblib.load("DBSCAN_model.joblib")
 # Streamlit app code
 st.write("Hello")
 
-# Get user input values using Streamlit widgets
-yellow = st.number_input('Enter the value for yellow:', min_value=0.0)
-red = st.number_input('Enter the value for red:', min_value=0.0)
-position_encoded = st.number_input('Enter the value for position_encoded:', step=1)
+# # Get user input values using Streamlit widgets
+# yellow = st.number_input('Enter the value for yellow:', min_value=0.0)
+# red = st.number_input('Enter the value for red:', min_value=0.0)
+# position_encoded = st.number_input('Enter the value for position_encoded:', step=1)
 
-# FastAPI app instance
+# # FastAPI app instance
 
 
-# Pydantic model for input data validation
-class InputFeatures(BaseModel):
-    yellow: float
-    red: float
-    position_encoded: int
+# # Pydantic model for input data validation
+# class InputFeatures(BaseModel):
+#     yellow: float
+#     red: float
+#     position_encoded: int
 
-# Endpoint for model prediction
-@app.post("/predict")
-async def predict(data: InputFeatures):
-    features = np.array([data.yellow, data.red, data.position_encoded]).reshape(1, -1)
-    prediction = model.predict(features)
-    return {"prediction": prediction.tolist()}
+# # Endpoint for model prediction
+# @app.post("/predict")
+# async def predict(data: InputFeatures):
+#     features = np.array([data.yellow, data.red, data.position_encoded]).reshape(1, -1)
+#     prediction = model.predict(features)
+#     return {"prediction": prediction.tolist()}
 
