931663ed066bd8d072368e8aae84d1eecbfe205e
abdulaziz <abdulaziz125000f@gmail.com>
2024-08-31 17:02:10 +0300

    Abdulaziz

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import joblib
 import streamlit as st
-import requests
 
 st.title("Machine Learning Prediction")
 
